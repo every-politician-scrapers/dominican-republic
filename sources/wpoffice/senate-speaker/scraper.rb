068f11e8e1e8aadbaf7ea42edc8f0a1980e2d09c
Tony Bowden <tony@tmtm.com>
2022-09-16 07:01:07 +0300

    compare Senate Presidents

---
+++ sources/wpoffice/senate-speaker/scraper.rb
@@ -0,0 +1,33 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Notes'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name dates].freeze
+    end
+
+    def raw_combo_date
+      years = super.tidy
+      years =~ /^\d{4}$/ ? "#{years} - #{years}" : years
+    end
+
+    def too_early?
+      !tds[1].text.match(/20\d\d/) || super
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
